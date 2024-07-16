3d4e0b0db0a1240514ff4d9f33279f75555923f5
Edwin Lin <edwinlin@uitc.com.tw>
2024-07-16 14:02:02 +0800

    feat 5.  add inputs
    checkbox radio

--- src/views/PraVue5.vue
+++ src/views/PraVue5.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 
 const text = ref('');
 
+const fruits = ref([]);
+
+const Picked = ref('');
+
 function onInput(e) {
   text.value = e.target.value;
 }
@@ -10,7 +14,37 @@ function onInput(e) {
 
 <template>
   <div style="margin-top: 5%">
-    <input style="font-size: 30px" :value="text" @input="onInput" placeholder="Type here" />
-    <p style="font-size: 30px; font-weight: 900">輸入的字串 :{{ text }}</p>
+    <input style="font-size: 20px" :value="text" @input="onInput" placeholder="Type here" />
+    <p style="font-size: 20px; font-weight: 900">輸入的字串 :{{ text }}</p>
+  </div>
+  <div style="font-size: 20px">
+    <h3>fruits : {{ fruits }}</h3>
+    <input type="checkbox" value="Apple" v-model="fruits" />
+    <label for="Apple">Apple</label>
+    <input type="checkbox" value="Banana" v-model="fruits" />
+    <label for="Banana">Banana</label>
+    <input type="checkbox" value="Lemon" v-model="fruits" />
+    <label for="Lemon">Lemon</label>
+  </div>
+  <div>
+    <h3 :class="Picked">Picked the color your class want</h3>
+    <input type="radio" value="yellow" v-model="Picked" />
+    <label for="yellow">yellow</label>
+    <input type="radio" value="red" v-model="Picked" />
+    <label for="red">red</label>
+    <input type="radio" value="blue" v-model="Picked" />
+    <label for="blue">blue</label>
   </div>
 </template>
+
+<style scoped>
+.red {
+  color: red;
+}
+.yellow {
+  color: yellow;
+}
+.blue {
+  color: blue;
+}
+</style>
